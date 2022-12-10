1e89ba5cea579e89073077377b218f053a322b73
Mauro Flores <mauroflores8193@gmail.com>
2022-12-10 11:41:34 -0500

    se mejora categories-router.js

--- delivery-service/src/categories/http/categories-router.js
+++ delivery-service/src/categories/http/categories-router.js
@@ -8,38 +8,28 @@ function createCategoriesRouter(manageCategoriesUsecase) {
   const router = express.Router();
 
   router.get("/categories", async (req, res) => {
-
     const categories = await manageCategoriesUsecase.getCategories();
     res.status(200).send(categories);
-
   });
 
   router.get("/categories/:id", async (req, res) => {
-
     const id = req.params.id;
     const book = await manageCategoriesUsecase.getCategory(id);
-    
     res.status(200).send(book);
-    
   });
-  
+
   router.post("/categories", async (req, res) => {
-    
-    validation = validateSchema(Category.schema, req);
-    
+    const validation = validateSchema(Category.schema, req);
     if (validation === true) {
       const book = await manageCategoriesUsecase.createCategory(req.body);
       res.status(201).send(book);
     } else {
       res.status(422).send(validation)
     }
-
   });
 
   router.put("/categories/:id", async (req, res) => {
-    
-    validation = validateSchema(Category.schema, req);
-    
+    const validation = validateSchema(Category.schema, req);
     if (validation === true) {
       const id = req.params.id;
       const book = await manageCategoriesUsecase.updateCategory(id, req.body);
@@ -47,16 +37,12 @@ function createCategoriesRouter(manageCategoriesUsecase) {
     } else {
       res.status(422).send(validation);
     }
-
   });
 
   router.delete("/categories/:id", async (req, res) => {
-
     const id = req.params.id;
     await manageCategoriesUsecase.deleteCategory(id);
-
     res.status(200).send(`Deleted ${id}`);
-
   });
 
   return router;
